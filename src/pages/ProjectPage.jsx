725c7320c13609b58c06189392fe25b00e943199
AkashParley <AkashParley@github.com>
2025-07-22 10:39:22 +0530

    Updated ProjectPage images and content

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -3,60 +3,59 @@ import Navbar from "../components/Navbar";
 
 const projects = [
   {
-  id: 1, // 
-  title: "Stocklyzer : Stock Analysis and Forecasting Web App using  Financial Models",
-  skills: "Python , Streamlit , Machine Learning , Data Visualization , Financial Models",
-  about: [
-    "A web app that allows users to analyze and forecast stock prices using financial models and machine learning.",
-    "The app uses historical stock data to train machine learning models and make predictions about future stock prices.",
-    "The app also includes a dashboard that allows users to visualize the stock data and the predictions.",
-    "The app is built using Streamlit and Python."
-  ],
-  image: "public/projects/Stocklyzer.png",
-  codeLink: "https://github.com/AkashParley/Stocklyzer",
-  liveLink: "https://stocklyzer-aqjkubjztydydfgepacyvt.streamlit.app/",
-},
+    id: 1,
+    title: "Stocklyzer : Stock Analysis and Forecasting Web App using  Financial Models",
+    skills: "Python , Streamlit , Machine Learning , Data Visualization , Financial Models",
+    about: [
+      "A web app that allows users to analyze and forecast stock prices using financial models and machine learning.",
+      "The app uses historical stock data to train machine learning models and make predictions about future stock prices.",
+      "The app also includes a dashboard that allows users to visualize the stock data and the predictions.",
+      "The app is built using Streamlit and Python."
+    ],
+    image: "/projects/project1.png",
+    codeLink: "https://github.com/AkashParley/Stocklyzer",
+    liveLink: "https://stocklyzer-aqjkubjztydydfgepacyvt.streamlit.app/",
+  },
   {
-  id: 2, // 
-  title: "IPL Data Analysis and Visualization",
-  skills: "Python , Pandas , Numpy , Matplotlib , Seaborn , Data Visualization , Tableau , Excel",
-  about: [
-    "A data analysis and visualization project on the Indian Premier League (IPL) dataset.",
-    "The project includes data cleaning, exploratory data analysis, and visualization using Python libraries like Pandas, Numpy, Matplotlib, and Seaborn.",
-    "Developed interactive Tableau dashboards to visualize batting, bowling, and overall team performance, enabling clear insights for data-driven decision-making.",
-    "Delivered insights that could support strategic team selection and performance optimization, showcasing how data-driven decisions improve competitive outcomes.",
-  
-  ],
-  image: "public/projects/IPL Logo.png",
-  codeLink: "https://github.com/AkashParley/IPL-Data-Analysis",
-  liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/IPLDataAnalysis_17393967228710/Dashboard1",
-},
- {
-  id: 3, // 
-  title: "Netflix Movies and TV Shows Data Analysis",
-  skills: "Excel , SQL , Data Visualization , Tableau",
-  about: [
-    "Data Analysis & Insights: Cleaned and analyzed 8,500+ Netflix titles using SQL, uncovering trends like a 15% annual release growth and dominant ratings (TV-MA for shows, PG-13 for movies).",
- 	"Interactive Visualizations: Built 10+ Tableau dashboards features showcasing regional content trends, rating distributions, and keyword categorizations.",
- 	"Business Impact: Delivered actionable insights to optimize content strategy, audience targeting, and regional market.",
-  ],
-  image: "public/projects/Netflix Logo Black Background.jpeg",
-  codeLink: "https://github.com/AkashParley/Netflix?tab=readme-ov-file",
-  liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/Book1_17287292047080/Netflix",
-},
-{
-  id: 4, // 
-  title: "Spotify Query Optimization and Data Analysis",
-  skills: "Excel , SQL , Data Visualization , Tableau",
-  about: [
-    "Data Cleaning & Preparation: Standardized a 50,000+ track dataset in Excel, ensuring consistency across artist names, album types, and attributes before PostgreSQL analysis.",
-    "Improved SQL query performance by 50% using indexing, CTEs, and window functions to analyze top tracks and streaming patterns.",
-    "Created dashboards revealing that 90% of top tracks were singles and energy levels drive genre-based streaming, supporting content strategy.",
-  ],
-  image: "public/projects/spotify logo.png",
-  codeLink: "https://github.com/AkashParley/Spotify",
-  liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/Book2_17289134476450/Dashboard1",
-},
+    id: 2,
+    title: "IPL Data Analysis and Visualization",
+    skills: "Python , Pandas , Numpy , Matplotlib , Seaborn , Data Visualization , Tableau , Excel",
+    about: [
+      "A data analysis and visualization project on the Indian Premier League (IPL) dataset.",
+      "The project includes data cleaning, exploratory data analysis, and visualization using Python libraries like Pandas, Numpy, Matplotlib, and Seaborn.",
+      "Developed interactive Tableau dashboards to visualize batting, bowling, and overall team performance, enabling clear insights for data-driven decision-making.",
+      "Delivered insights that could support strategic team selection and performance optimization, showcasing how data-driven decisions improve competitive outcomes.",
+    ],
+    image: "/projects/project2.png",
+    codeLink: "https://github.com/AkashParley/IPL-Data-Analysis",
+    liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/IPLDataAnalysis_17393967228710/Dashboard1",
+  },
+  {
+    id: 3,
+    title: "Netflix Movies and TV Shows Data Analysis",
+    skills: "Excel , SQL , Data Visualization , Tableau",
+    about: [
+      "Data Analysis & Insights: Cleaned and analyzed 8,500+ Netflix titles using SQL, uncovering trends like a 15% annual release growth and dominant ratings (TV-MA for shows, PG-13 for movies).",
+      "Interactive Visualizations: Built 10+ Tableau dashboards features showcasing regional content trends, rating distributions, and keyword categorizations.",
+      "Business Impact: Delivered actionable insights to optimize content strategy, audience targeting, and regional market.",
+    ],
+    image: "/projects/project3.jpeg",
+    codeLink: "https://github.com/AkashParley/Netflix?tab=readme-ov-file",
+    liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/Book1_17287292047080/Netflix",
+  },
+  {
+    id: 4,
+    title: "Spotify Query Optimization and Data Analysis",
+    skills: "Excel , SQL , Data Visualization , Tableau",
+    about: [
+      "Data Cleaning & Preparation: Standardized a 50,000+ track dataset in Excel, ensuring consistency across artist names, album types, and attributes before PostgreSQL analysis.",
+      "Improved SQL query performance by 50% using indexing, CTEs, and window functions to analyze top tracks and streaming patterns.",
+      "Created dashboards revealing that 90% of top tracks were singles and energy levels drive genre-based streaming, supporting content strategy.",
+    ],
+    image: "/projects/project4.png",
+    codeLink: "https://github.com/AkashParley/Spotify",
+    liveLink: "https://public.tableau.com/app/profile/akash.parley/viz/Book2_17289134476450/Dashboard1",
+  },
 ];
 
 const ProjectCard = ({ project }) => {
